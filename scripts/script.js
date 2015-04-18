5592b54c671561d30c91af1059b93309e2918300
zohzik3 <taymyr1026@gmail.com>
2015-04-18 20:21:31 +0000

    asd

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,8 @@ function gameFeild(){
     //the in-game pages
     pages : {
       inventoryPage : document.getElementById("inventory-page"),
-      storePage : document.getElementById("store-page")
+      storePage : document.getElementById("store-page"),
+      peoplePage : document.getElementById("people-page")
     }
     
   };
@@ -29,9 +30,16 @@ function gameFeild(){
   //array that is filled with all the different store items in-game
   this.store = [];
   
+  //array that is filled with all the different people in-game
+  this.people = [];
+  
   //timers, carrier variables
   this.others = {
-    activePage : "inventory"
+    activePage : "inventory",
+    collectionTimer : function(){
+      update();
+      window.setTimeout(function(){Game.others.collectionTimer();}, 1000);
+    }
   };
   
   //player object
@@ -46,6 +54,17 @@ function gameFeild(){
     //array of items bought in the store
     storeItemsBought : [0]
   };
+}
+
+//update, money collection
+function update(){
+  for(var i = 0; i < Game.people.length; i++){
+    for(var w = 0; w < Game.people[i].qty; w++){
+      for(var q = 0; q < Game.people[i].mineTimes; q++){
+        mineWithAxe();
+      }
+    }
+  }
 }
 
 //basic ore object, includes links to repective html document box.
@@ -76,12 +95,34 @@ function initItem(name, cost, boxLink, effect){
   Game.store[Game.store.length] = new item(name, cost, boxLink, effect);
 }
 
+//basic people object, includes links to respective html document box
+function people(name, cost, qty, maxQty, mineTimes, costIncrease, amountLink, maxAmountLink, costLink, mineTimesLink, effect){
+  this.name = name;
+  this.cost = cost;
+  this.qty = qty;
+  this.maxQty = maxQty;
+  this.mineTimes = mineTimes;
+  this.costIncrease = costIncrease;
+  this.amountLink = amountLink;
+  this.maxAmountLink = maxAmountLink;
+  this.costLink = costLink;
+  this.mineTimesLink = mineTimesLink;
+  this.effect = effect;
+}
+
+//initilizes people, and puts it inside the Game.people array
+function initPeople(name, cost, qty, maxQty, mineTimes, costIncrease, amountLink, maxAmountLink, costLink, mineTimesLink, effect){
+  Game.people[Game.people.length] = new people(name, cost, qty, maxQty, mineTimes, costIncrease, amountLink, maxAmountLink, costLink, mineTimesLink, effect);
+}
+
 //asks user for username, loads gameFeild, then loads all other respective elements
 window.onload = function(){
   document.getElementById("player-name-space-insert").innerHTML = "testing"; //prompt("What is your name?", "Max nine characters");
   window.Game = new gameFeild();
   loadInventory();
   loadStore();
+  loadPeople();
+  Game.others.collectionTimer();
 };
 
 //calls initOre on each ore
@@ -96,6 +137,15 @@ function loadInventory(){
 function loadStore(){
   initItem("Pickaxe efficiency x2", 50, document.getElementById("store-box-0"), function(){
     Game.player.variables.minePerClick*=2;
+  });
+}
+
+//calls initPeople on each people item
+function loadPeople(){
+  initPeople("Miner", 100, 0, 5, 1, 1.45, document.getElementById("people-box-0-amount"), document.getElementById("people-box-0-max-amount"), document.getElementById("people-box-0-cost"), document.getElementById("people-box-0-per-sec"), function(){
+    if(Game.people[0].qty===1){
+      //unlocks option for level 2 upgrades
+    }
   });
 }
 
@@ -185,6 +235,31 @@ function buyItemBypass(id){
   Game.store[id].boxLink.className = "na";
 }
 
+//buys person
+function buyPeople(id){
+  if(Game.currency.gold >= Game.people[id].cost && Game.people[id].qty < Game.people[id].maxQty){
+    Game.currency.gold -= Game.people[id].cost;
+    Game.currency.gold = fixNum(Game.currency.gold);
+    Game.htmlLinks.icons.goldIcon.innerHTML = Game.currency.gold;
+    Game.people[id].qty++;
+    Game.people[id].cost*=Game.people[id].costIncrease;
+    Game.people[id].cost = fixNum(Game.people[id].cost);
+    Game.people[id].amountLink.innerHTML = Game.people[id].qty;
+    Game.people[id].costLink.innerHTML = Game.people[id].cost;
+    Game.people[id].effect();
+  }
+}
+
+//buys person, bypassing the cost, and max qty (used for loading, since other effects and upgrades may not be in place to raise max qty yet in loading)
+function buyPeopleBypass(id){
+  Game.people[id].qty++;
+  Game.people[id].cost*=Game.people[id].costIncrease;
+  Game.people[id].cost = fixNum(Game.people[id].cost);
+  Game.people[id].amountLink.innerHTML = Game.people[id].qty;
+  Game.people[id].costLink.innerHTML = Game.people[id].cost;
+  Game.people[id].effect();
+}
+
 //changes what page is being displayed
 function cyclePage(pageName){
   if(pageName !== Game.others.activePage){
@@ -204,7 +279,7 @@ function giveCurrency(currency, amount){
 //debugging and testing function, gives ores
 function giveOre(ore, amount){
   for(var i = 0; i < Game.inventory.length; i++){
-    if(ore === Game.inventory[i].name){
+    if(ore.toLowerCase() === Game.inventory[i].name.toLowerCase()){
       Game.inventory[i].qty += amount;
       Game.inventory[i].boxLink.className = "box";
       Game.inventory[i].amountLink.innerHTML = Game.inventory[i].qty;
@@ -216,8 +291,18 @@ function giveOre(ore, amount){
 //debugging and testing function, gives store item
 function giveItem(item){
   for(var i = 0; i < Game.store.length; i++){
-    if(item === Game.store[i].name){
+    if(item.toLowerCase() === Game.store[i].name.toLowerCase()){
       buyItemBypass(i);
+      return;
+    }
+  }
+}
+
+//debugging and testing function, gives people item
+function givePeople(people){
+  for(var i = 0; i < Game.people.length; i++){
+    if(people.toLowerCase() === Game.people[i].name.toLowerCase()){
+      buyPeopleBypass(i);
       return;
     }
   }
